f77dbae11db6f28954497d14f05c57188daf6e3a
Steven <Steven.Hamilton@outlook.com>
2017-02-25 01:24:46 -0700

    add the stuff

--- TestProject/TestProject/TestProject.cpp
+++ TestProject/TestProject/TestProject.cpp
@@ -2,14 +2,10 @@
 //
 
 #include "stdafx.h"
-#include <iostream>
-using namespace std;
+
 
 int main()
 {
-	string ok = "hi";
-	string neatstuff = "neatstuff";
-	string heh = "ok";
     return 0;
 }
 
